acedc60d3de3a1d2f63ff939e560ded4da4bc164
FollowWang <follow_wang@outlook.com>
2015-06-27 15:51:04 +0800

    new file

--- UIImageView/UIImageView/ViewController.m
+++ UIImageView/UIImageView/ViewController.m
@@ -17,6 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *mars = [UIImage imageNamed:@"1.png"];
+    NSLog(@"image:%@",mars);
+    CGFloat capw = mars.size.width/2.0 -1;
+    CGFloat caph = mars.size.height/2.0 -1;
+    UIImage *marsTiled = [mars resizableImageWithCapInsets:UIEdgeInsetsMake(caph, capw, caph, capw)];
+    
+    UIImageView *iv = [[UIImageView alloc] initWithFrame:CGRectMake(20, 5, mars.size.width*2, mars.size.height*1.5)];
+    iv.image = marsTiled;
+    //UIImageView *iv = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"1.png"]];
+    [self.view addSubview:iv];
+    iv.center = self.view.center;
 }
 
 - (void)didReceiveMemoryWarning {
